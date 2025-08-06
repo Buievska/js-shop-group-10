c920f763ff1d900bb5b4aad4edc26b5c55048692
Ania <anna111239@gmail.com>
2025-08-06 03:28:26 +0200

    repository config

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header.js';
+import './js/furniture-list.js';
+import './js/popular-product.js';
+import './js/faq.js';
+import './js/feedback.js';
+import './js/furniture-modal.js';
+import './js/order-modal.js';
