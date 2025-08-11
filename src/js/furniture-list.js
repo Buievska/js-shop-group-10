4cd1fa84cbe12af230d42585930cb0ba50d60136
Alexsandr-Kharchenko <googfather8@gmail.com>
2025-08-11 19:24:06 +0300

    fix error 404

--- src/js/furniture-list.js
+++ src/js/furniture-list.js
@@ -46,6 +46,7 @@ async function getFurnitures(page = 1, category = '') {
 
 // --- Рендер категорій ---
 function getImageUrl(name) {
+  if (!name) name = 'vsi-tovary.png';
   return new URL(`../img/furniture-list/${name}`, import.meta.url).href;
 }
 
